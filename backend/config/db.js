505ec76871f2b23a00fad261e0319d5c1bcf3272
zencoder06 <ankitgupta.odb09@gmail.com>
2026-06-23 20:19:40 +0530

    Database Connected

---
+++ backend/config/db.js
@@ -0,0 +1,8 @@
+import mongoose from "mongoose";
+
+export const connectDB=async()=>{
+  mongoose.connect("mongodb+srv://ankitguptaodb09_db_user:QyZ9Cym4fjnyJOj3@cluster0.xjsjk1u.mongodb.net/Library Management").then(()=>{
+    console.log("DB Connected");
+  })
+
+}
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,18 @@
 import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
+import { connectDB } from './config/db.js';
 
 const PORT=5000;
 const app=express();
+
+app.use(cors());
+app.use(express.json());
+
+
+connectDB();
+
+
 
 app.get("/",(req,res)=>{
   res.send("API WORKING");
