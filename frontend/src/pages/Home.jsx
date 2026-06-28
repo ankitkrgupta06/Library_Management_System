c0d4ea596779ea90f9fd3cbc0a8274a8f5a32fcb
zencoder06 <ankitgupta.odb09@gmail.com>
2026-06-28 23:35:51 +0530

    Minor change

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Sidebar from '../components/Sidebar'
+
+function Home() {
+  return (
+    <div>
+      <Sidebar title="ShelfWise" subtitle="Library management portal" />
+    </div>
+  )
+}
+
+export default Home
