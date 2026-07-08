902bfe96e6ab8b5d77483dddfe51630f6fc51d9e
zencoder06 <ankitgupta.odb09@gmail.com>
2026-07-08 22:08:43 +0530

    Created the AdminDashboard

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,11 +4,14 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { AuthProvider } from './shared/AuthContext'
+import { LibraryProvider } from './shared/LibraryContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
   <AuthProvider>
+    <LibraryProvider>
     <App />
+    </LibraryProvider>
   </AuthProvider>
     
   </BrowserRouter>,
